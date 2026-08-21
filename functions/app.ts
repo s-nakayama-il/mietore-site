793a8ef50d3a50fc511cbad41b77948f2898df39
nakayama <s-nakayama@i-linksnet.com>
2026-08-21 16:02:11 +0900

    fix: /app を onRequest に変更し HEAD も302

--- functions/app.ts
+++ functions/app.ts
@@ -1,6 +1,6 @@
 import { storeUrlFor } from '../src/lib/store';
 
-export const onRequestGet: PagesFunction = async ({ request }) => {
+export const onRequest: PagesFunction = async ({ request }) => {
   const target = storeUrlFor(request.headers.get('user-agent'));
   const location = target.startsWith('/') ? new URL(target, request.url).toString() : target;
   return Response.redirect(location, 302);
